40263cb829e4b6ab0712c77e51a206bf31aca8a3
SuaraSamad <abdulsamadsuara@gmail.com>
2022-12-12 10:14:28 +0100

    Added a new function

--- # pythontest2.py
+++ # pythontest2.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import datetime
 
 dog = namedtuple('dog',['name','age','breed'])
 scoob = dog(name='scooby',age=8,breed='rottweiler')
@@ -7,3 +8,11 @@ x = scoob.name
 b = scoob.breed
 print(x)
 print(b)
+
+# creating class with turple
+
+def wish_birthday():
+    if datetime.date.today == datetime.date.today:
+        print("happy birthday")
+    else:
+        pass
