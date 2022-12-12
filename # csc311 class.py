b71a1afe9237ead6555481d0bfb59ec47d542678
SuaraSamad <abdulsamadsuara@gmail.com>
2022-12-12 10:27:58 +0100

    updated the function

--- # csc311 class.py
+++ # csc311 class.py
@@ -1,7 +1,9 @@
 count = 0
+txt = "quduywf  euyuygGUFGVHJBB"
+for letter in txt:
+    if letter == letter.upper():
+        count+=1
+        if letter == " ":
+            count-=1
 
-answer = input('enter? ')
-if answer == answer.upper():
-    print('go')
-
-def 
+print(count)
